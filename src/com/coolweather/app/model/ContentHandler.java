4a092f25ba8288924508890b96125b76c7950005
leiqingqiang <leiqingqiang@qq.com>
2014-09-23 20:21:41 +0800

    complete province city county

---
+++ src/com/coolweather/app/model/ContentHandler.java
@@ -0,0 +1,63 @@
+package com.coolweather.app.model;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import com.coolweather.app.db.CoolWeatherDB;
+
+import android.content.Context;
+
+public class ContentHandler extends DefaultHandler {
+
+	private CoolWeatherDB coolWeatherDB;
+	private int autoIncrementProvince = 0;
+	private int autoIncrementCity = 0;
+	
+	public ContentHandler(Context context) {
+		coolWeatherDB = CoolWeatherDB.getInstance(context);
+	}
+	
+	@Override
+	public void startDocument() throws SAXException {
+	}
+	
+	@Override
+	public void startElement(String uri, String localName, String qName,
+			Attributes attributes) throws SAXException {
+		if("province".equals(localName)) {
+			autoIncrementProvince++;
+			Province province = new Province();
+			province.setProvinceCode(attributes.getValue(0));
+			province.setProvinceName(attributes.getValue(1));
+			coolWeatherDB.saveProvince(province);
+		} else if("city".equals(localName)) {
+			autoIncrementCity++;
+			City city = new City();
+			city.setCityCode(attributes.getValue(0));
+			city.setCityName(attributes.getValue(1));
+			city.setProvinceId(autoIncrementProvince);
+			coolWeatherDB.saveCity(city);
+		} else if("county".equals(localName)) {
+			County county = new County();
+			county.setCountyCode(attributes.getValue(0));
+			county.setCountyName(attributes.getValue(1));
+			county.setCityId(autoIncrementCity);
+			coolWeatherDB.saveCounty(county);
+		}
+	}
+	
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		
+	}
+	
+	@Override
+	public void endDocument() throws SAXException {
+	}
+
+}
---
+++ src/com/coolweather/app/util/HttpUtil.java
@@ -0,0 +1,65 @@
+package com.coolweather.app.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpUtil {
+
+	public static void sendHttpRequest(final String address, final HttpCallbackListener listener) {
+//		if(!isNetworkAvailable()) {
+//			Toast.makeText(BoxApplication.getContext(), "network is unavailable", 
+//					Toast.LENGTH_SHORT).show();
+//			return;
+//		}
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				HttpURLConnection conn = null;
+				try {
+					URL url = new URL(address);
+					conn = (HttpURLConnection) url.openConnection();
+					conn.setRequestMethod("GET");
+					conn.setConnectTimeout(8 * 1000);
+					conn.setReadTimeout(8 * 1000);
+					conn.setDoInput(true);
+					conn.setDoOutput(true);
+					InputStream in = conn.getInputStream();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+					StringBuilder response = new StringBuilder();
+					String line = null;
+					while((line = reader.readLine()) != null) {
+						response.append(line);
+					}
+					if(listener != null) {
+						//回调onFinish()方法
+						listener.onFinish(response.toString());
+					}
+				} catch (Exception e) {
+					if(listener != null) {
+						//回调onError()方法
+						listener.onError(e);
+					}
+				} finally {
+					if(conn != null) {
+						conn.disconnect();
+					}
+				}
+			}
+		}).start();
+	}
+	
+	/*private static boolean isNetworkAvailable() {
+		ConnectivityManager connectivityManager = (ConnectivityManager) BoxApplication.getContext()
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+		if(networkInfo != null && networkInfo.isAvailable()) {
+			return true;
+		} else {
+			return false;
+		}
+	}*/
+}
